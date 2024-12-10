e79856f7ee6659bf85c61ef4588df20d206eafdf
Devon Proctor <devon.proctor@gmail.com>
2024-12-10 12:46:45 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Trashy data
+
+## Data
+
+Data previously, available at <https://data.sfgov.org/City-Infrastructure/DPW-Street-Sidewalk-Evaluation-Results-CY22/fsqv-4vqv/about_data>, updated November 20, 2024, but data only goes through 4/25/23 :/
+
+Updated dataset is now hosted at <https://data.sfgov.org/City-Infrastructure/Street-Sidewalk-Maintenance-Standards-Results-Jan-/qya8-uhsz/about_data>.
---
+++ flake.nix
@@ -0,0 +1,81 @@
+{
+  description = "Data analysis of SF trash audit data.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+
+          narwhals = pkgs.python311Packages.buildPythonPackage rec {
+            pname = "narwhals";
+            version = "1.8.1";
+
+            pyproject = true;
+            src = pkgs.python3Packages.fetchPypi {
+              inherit pname version;
+              hash = "sha256-l1J3eOEfOaHl4hE7j7uerXiL5BwDN/IYUuaE43j1g+g=";
+            };
+            dependencies = [ pkgs.python311Packages.hatchling ];
+          };
+
+          python = let
+            packageOverrides = self: super: {
+              altair = super.altair.overridePythonAttrs(old: rec {
+                pname = "altair";
+                version = "5.4.1";
+                src =  pkgs.python3Packages.fetchPypi {
+                  inherit pname version;
+                  hash = "sha256-DOjC5mVGyzJ+Xy11cuwOfG/uzoFiAyFWE5YvDsHXaoI=";
+                };
+                dependencies = [
+                  narwhals
+                  pkgs.python311Packages.typing-extensions
+                  pkgs.python311Packages.jinja2
+                  pkgs.python311Packages.jsonschema
+                ];
+                doCheck = false;
+
+              });
+            };
+          in pkgs.python311.override {inherit packageOverrides; self = python;};
+
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            name = "sf election forecasting";
+            packages = [
+              (python.withPackages (p: with p; [
+                pynvim
+
+                altair
+                geopandas
+                jupyter
+                jupyter-cache
+                matplotlib
+                pandas
+                polars
+                pyarrow
+                pyparsing
+                scikit-learn
+                seaborn
+                shap
+                xgboost
+              ]))
+            ];
+
+            shellHook = ''
+              export DEV_ENVIRONMENT="trashy-data"
+            '';
+          };
+        }
+      );
+}
